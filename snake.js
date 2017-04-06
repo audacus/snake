816898406e49c6cb232771b2d7197431d0d03cdc
audacus <david.burkhart@hotmail.com>
2017-04-06 20:11:34 +0200

    +engine class and test

--- snake.js
+++ snake.js
@@ -1,5 +1,18 @@
 'use strict';
 
+function Engine(options) {
+    window.onkeydown = function (e) {
+        var code = e.keyCode ? e.keyCode : e.which;
+        console.log(code)
+    };
+    
+    this.renderFrame = function() {
+        console.log('render');
+    };
+    
+    this.interval = setInterval(this.renderFrame, options.interval);
+}
+
 function Dimension(options) {
     this.x = options.x || 0;
     this.y = options.y || 0;
@@ -7,11 +20,18 @@ function Dimension(options) {
 }
 
 function Field(options) {
-    this.dimensions = new Dimension(options.dimensions);
+    this.dimensions = new Dimension(options.dimensions || {});
 }
 
 function Snake(options) {
-    this.field = new Field(options.field);
+    this.field = new Field(options.field || {});
     this.interval = options.interval || 500;
     this.bonus = options.bonus || 1;
+    this.engine = new Engine({
+        interval: this.interval
+    });
+    
+    
 }
+
+new Snake({});
